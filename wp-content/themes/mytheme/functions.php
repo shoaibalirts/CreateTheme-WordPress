d2c6399cb190d2c574ef9620d17f0ab0ff174fcf
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-31 12:15:27 +0100

    added tailwindcss via functions.php

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -7,8 +7,8 @@
     /**
  * Enqueue scripts and styles.
  */
-function cg_your_theme_scripts() {
+function university_theme_scripts() {
 	wp_enqueue_style( 'output', get_template_directory_uri() . '/dist/output.css', array() );
 }
-add_action( 'wp_enqueue_scripts', 'cg_your_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'university_theme_scripts' );
 ?>
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
-</head>
-<body>
-    <h1 class="bg-red-500">Hello Theme!</h1>
-</body>
-</html>
 <?php
     echo "Hello World!";
 ?>
