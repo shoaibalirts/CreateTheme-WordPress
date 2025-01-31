d532c4b5125b2cfb7f631c67423202ea9be84086
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-31 11:20:41 +0100

    tested tailwindcss is enqued via functions.php

---
+++ wp-content/themes/mytheme/functions.php
@@ -0,0 +1,14 @@
+<?php
+    // function load_css(){
+    //     wp_register_style('my-plugin', plugins_url('/css/style.css'));
+    //     wp_enque_style('my-plugin');
+    // }
+
+    /**
+ * Enqueue scripts and styles.
+ */
+function cg_your_theme_scripts() {
+	wp_enqueue_style( 'output', get_template_directory_uri() . '/dist/output.css', array() );
+}
+add_action( 'wp_enqueue_scripts', 'cg_your_theme_scripts' );
+?>
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1 class="bg-red-500">Hello Theme!</h1>
+</body>
+</html>
 <?php
     echo "Hello World!";
 ?>
