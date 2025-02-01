c39b01200d14be8529dceec5c24bb2f4bfc991bc
shoaibalirts <shoaibali.rts@gmail.com>
2025-02-01 17:35:22 +0100

    revisited custom post type for Programs

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,14 +1,78 @@
 <?php
-    // function load_css(){
-    //     wp_register_style('my-plugin', plugins_url('/css/style.css'));
-    //     wp_enque_style('my-plugin');
-    // }
-
-    /**
- * Enqueue scripts and styles.
- */
-function university_theme_scripts() {
+   
+function university_theme_styles_scripts() {
 	wp_enqueue_style( 'output', get_template_directory_uri() . '/dist/output.css', array() );
 }
-add_action( 'wp_enqueue_scripts', 'university_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'university_theme_styles_scripts' );
+
+
+
+function create_university_programs() {
+    register_post_type('program',
+        array(
+            'labels'      => array(
+                'name'          => 'Programs',
+                'singular_name' => 'Program',
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'supports'    => array('title', 'editor', 'thumbnail', 'excerpt'),
+            'show_in_rest' => true, // Enables REST API support
+        )
+    );
+}
+add_action('init', 'create_university_programs');
+
+
+function register_courses_post_type() {
+    register_post_type('course', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Courses',
+            'singular_name' => 'Course'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')
+    ));
+}
+add_action('init', 'register_courses_post_type');
+
+function register_professors_post_type() {
+    register_post_type('professor', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')
+    ));
+}
+add_action('init', 'register_professors_post_type');
+
+function register_news_post_type() {
+    register_post_type('news', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'News',
+            'singular_name' => 'news'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')
+    ));
+}
+add_action('init', 'register_news_post_type');
+
+function register_events_post_type() {
+    register_post_type('course', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')
+    ));
+}
+add_action('init', 'register_events_post_type');
 ?>
