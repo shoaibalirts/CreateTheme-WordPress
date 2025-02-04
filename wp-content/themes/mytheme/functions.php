3ed69c6355fc3ce3d8e471eaf9559e0e64006746
shoaibalirts <shoaibali.rts@gmail.com>
2025-02-04 19:32:20 +0100

    last commit today 04-02-2025

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -6,6 +6,34 @@ function university_theme_styles_scripts() {
 add_action( 'wp_enqueue_scripts', 'university_theme_styles_scripts' );
 
 
+function university_customize_register($wp_customize) {
+    $wp_customize->add_section('university_logo_section', array(
+        'title'    => __('University Logo', 'mytheme'),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting('university_logo');
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'university_logo', array(
+        'label'    => __('Upload University Logo', 'mytheme'),
+        'section'  => 'university_logo_section',
+        'settings' => 'university_logo',
+    )));
+}
+add_action('customize_register', 'university_customize_register');
+
+
+function university_logo_rest_api() {
+    return get_theme_mod('university_logo');
+}
+add_action('rest_api_init', function() {
+    register_rest_route('custom/v1', '/university-logo', array(
+        'methods'  => 'GET',
+        'callback' => 'university_logo_rest_api',
+    ));
+});
+
+
 
 function create_university_programs() {
     register_post_type('program',
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Hello World!";
+    echo "You are redirected to index.php";
 ?>
